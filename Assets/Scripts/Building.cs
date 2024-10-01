a1a2c6f5adec89ec9cea0b47008e34b88a221aeb
Elou2000 <158079436+Elou2000@users.noreply.github.com>
2024-10-01 19:56:43 +0200

    Spawn batiment + zone verte
    j'ai reglé le problème la zone verte apparait, je vais faire le posage et les zones rouges au prochain commit

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public bool Placed { get; private set; }
+    public BoundsInt area;
+
+    #region Build Methods
+
+
+
+    #endregion
+}
