b46386ab6d561f3a6d8e6569e724318c0ec68c84
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-10-01 15:40:59 +0200

    ressources manbager

---
+++ Assets/Scripts/RessourcesManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourcesManager : MonoBehaviour
+{
+    [Header("game stats")] 
+    public float chrono;
+    public float wave;
+    
+    [Header("player stats")] 
+    public float health;
+    
+    [Header("ressources")]
+    public int wood;
+    public int stone;
+
+    [Header("souls ressources")] 
+    public float fireSoul;
+    public float iceSoul;
+    public float electricSoul;
+}
