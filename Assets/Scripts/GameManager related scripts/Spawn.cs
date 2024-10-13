3dde13b71403f43d0dfa301d7fcd9d9b8e77751f
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-10-13 15:59:03 +0200

    spawn

--- Assets/Scripts/GameManager related scripts/Spawn.cs
+++ Assets/Scripts/GameManager related scripts/Spawn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawn : MonoBehaviour
@@ -11,18 +12,49 @@ public class Spawn : MonoBehaviour
     public int numberOfMonsterTwo;
     public GameObject monsterTypeOne;
     public GameObject monsterTypeTwo;
-    
+
+    public float SpawnCircleRadius;
+
+    public void ButtonFonctionLaunchWave()
+    {
+        hasToSpawn = true;
+    }
 
     void Update()
     {
         if (hasToSpawn == true && isAlreadySpawning == false)
         {
-            LaunchWave(waveNumber, numberOfMonsterOne,numberOfMonsterTwo);
+            isAlreadySpawning = true;
+            LaunchWave();
+            isAlreadySpawning = false;
+            waveNumber++;
+
         }
+        
     }
 
-    void LaunchWave(int waveNumber, int numberOfMonssterOne, int numberOfMonsterTwo);
+    public void LaunchWave()
     {
+        for(var i = 0; i < numberOfMonsterOne; i++)
+        {
+            float x = Random.Range(-1 * SpawnCircleRadius, SpawnCircleRadius);
+            x -= x % 1;
+            float y = Random.Range(-1 * SpawnCircleRadius, SpawnCircleRadius);
+            y -= y % 1;
+            GameObject newMonster = Instantiate(monsterTypeOne, new Vector2(x,y), Quaternion.identity);
+            newMonster.GetComponent<MonsterDeathBehaviour>().RM = this.gameObject.GetComponent<RessourcesManager>();
+        }
         
+        for(var i = 0; i < numberOfMonsterTwo; i++)
+        {
+            float x = Random.Range(-1 * SpawnCircleRadius, SpawnCircleRadius);
+            x -= x % 1;
+            float y = Random.Range(-1 * SpawnCircleRadius, SpawnCircleRadius);
+            y -= y % 1;
+            GameObject newMonster = Instantiate(monsterTypeTwo, new Vector2(x,y), Quaternion.identity);
+            newMonster.GetComponent<MonsterDeathBehaviour>().RM = this.gameObject.GetComponent<RessourcesManager>();
+        }
+
+        hasToSpawn = false;
     }
 }
