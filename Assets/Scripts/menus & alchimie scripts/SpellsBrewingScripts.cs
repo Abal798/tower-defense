f57c11fe4267f4f33eb2324d16dccf2b24c946e0
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-10-28 15:59:12 +0100

    Update SpellsBrewingScripts.cs

--- Assets/Scripts/menus & alchimie scripts/SpellsBrewingScripts.cs
+++ Assets/Scripts/menus & alchimie scripts/SpellsBrewingScripts.cs
@@ -10,6 +10,8 @@ public class SpellsBrewingScripts : MonoBehaviour
     public TextMeshProUGUI fireSoulDisplay;
     public TextMeshProUGUI waterSoulDisplay;
     public TextMeshProUGUI earthSoulDisplay;
+
+    public string brewedSpell = "";
     
     
     void Start()
@@ -23,6 +25,34 @@ public class SpellsBrewingScripts : MonoBehaviour
         fireSoulDisplay.text = "fire : " + RM.fireSoul.ToString();
         waterSoulDisplay.text = "water : " + RM.waterSoul.ToString();
         earthSoulDisplay.text = "earth : " + RM.plantSoul.ToString();
+    }
+
+    public void FireButtonSelected()
+    {
+        if (brewedSpell.Length < 3)
+        {
+            RM.fireSoul -= 10;
+            brewedSpell += "F";
+        }
+        
+    }
+
+    public void WaterButtonSelected()
+    {
+        if (brewedSpell.Length < 3)
+        {
+            RM.waterSoul -= 10;
+            brewedSpell += "W";
+        }
+    }
+
+    public void EarthButtonSelected()
+    {
+        if (brewedSpell.Length < 3)
+        {
+            RM.plantSoul -= 10;
+            brewedSpell += "E";
+        }
     }
 
 
