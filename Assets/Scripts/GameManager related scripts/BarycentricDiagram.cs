fd54853c94b3d27655687f20fc72a92dddeb4746
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-11-06 15:41:14 +0100

    triangle diagram non intégré

---
+++ Assets/Scripts/GameManager related scripts/BarycentricDiagram.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class BarycentricDiagram : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public Transform pointC;
+    public Transform pointMarker;
+    
+
+    void CalculatePosition(int weightA, int weightB, int weightC)
+    {
+        int totalWeight = weightA + weightB + weightC;
+
+        if (totalWeight > 0)
+        {
+            float a = (float)weightA / totalWeight;
+            float b = (float)weightB / totalWeight;
+            float c = (float)weightC / totalWeight;
+            
+            Vector3 barycentricPosition = a * pointA.position + b * pointB.position + c * pointC.position;
+            pointMarker.position = barycentricPosition;
+        }
+    }
+}
