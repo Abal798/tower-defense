bd0e7eb705cba01206ab3f6bf7446d52f5b2abe0
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-11-06 10:56:35 +0100

    monstres pokemons

--- Assets/Scripts/Monster scripts/MonsterStats.cs
+++ Assets/Scripts/Monster scripts/MonsterStats.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class MonsterStats : MonoBehaviour
 {
+    public int health;
     public float basicSpeed = 3;
     public float speed;
     public float damages;
+    public int type;
+    public float damageFactorbonus = 1.5f;
+    public float damageFactorMalus = 0.75f;
     void Start()
     {
+        Renderer renderer = GetComponent<Renderer>();
         speed = basicSpeed;
+        
+        if (type == 1)
+        {
+            renderer.material.color = Color.red;
+        }
+        if (type == 2)
+        {
+            renderer.material.color = Color.blue;
+        }
+        if (type == 3)
+        {
+            renderer.material.color = Color.green;
+        }
     }
 
     // Update is called once per frame
