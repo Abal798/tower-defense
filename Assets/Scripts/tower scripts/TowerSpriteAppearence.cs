4f54c3f988e5fe72f103c7f68e70811be08a975c
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-11-18 18:24:09 +0100

    les tours ont des couleurs

--- Assets/Scripts/tower scripts/TowerSpriteAppearence.cs
+++ Assets/Scripts/tower scripts/TowerSpriteAppearence.cs
@@ -8,13 +8,24 @@ public class TowerSpriteAppearence : MonoBehaviour
     private Vector3 targetSize;
     private bool isChangingSize = false;
     
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+    public int currentSprite;
 
+
+    public void Start()
+    { 
+        ChangeSprite();
+    }
+    
+    
     public void UpdateSize(float duration,float scaleFactor, Transform objectToChange)
     {
         if (!isChangingSize)
         {
             StartCoroutine(ScaleOverTime(duration, scaleFactor, objectToChange));
         }
+        ChangeSprite();
     }
 
     IEnumerator ScaleOverTime(float time, float scaleFactor,Transform objectToChange)
@@ -36,5 +47,68 @@ public class TowerSpriteAppearence : MonoBehaviour
 
         objectToChange.localScale = targetScale;
         isChangingSize = false;
+    }
+
+    void ChangeSprite()
+    {
+        List<int> currentAmeliration = transform.GetComponentInParent<TowerStats>().ameliorations;
+        if (currentAmeliration.Count == 1)
+        {
+            currentSprite = currentAmeliration[0];
+        }
+        else if (currentAmeliration.Count == 2)
+        {
+            if (currentAmeliration[0] == 1)
+            {
+                if(currentAmeliration[1] == 1)
+                {
+                    currentSprite = 4;
+                }
+                else if (currentAmeliration[1] == 2)
+                {
+                    currentSprite = 5;
+                }
+                else if (currentAmeliration[1] == 3)
+                {
+                    currentSprite = 6;
+                }
+            }
+            if (currentAmeliration[0] == 2)
+            {
+                if(currentAmeliration[1] == 1)
+                {
+                    currentSprite = 5;
+                }
+                else if (currentAmeliration[1] == 2)
+                {
+                    currentSprite = 7;
+                }
+                else if (currentAmeliration[1] == 3)
+                {
+                    currentSprite = 8;
+                }
+            }
+            if (currentAmeliration[0] == 3)
+            {
+                if(currentAmeliration[1] == 1)
+                {
+                    currentSprite = 6;
+                }
+                else if (currentAmeliration[1] == 2)
+                {
+                    currentSprite = 8;
+                }
+                else if (currentAmeliration[1] == 3)
+                {
+                    currentSprite = 9;
+                }
+            }
+        }
+        else
+        {
+            currentSprite = 0;
+        }
+
+        spriteRenderer.sprite = spriteArray[currentSprite];
     }
 }
