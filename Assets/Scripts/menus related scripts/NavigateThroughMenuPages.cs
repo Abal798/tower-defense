a0212c3af99e426c978180b1a88b7aabc752ebca
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-11-19 16:21:12 +0100

    pages de menus etc

---
+++ Assets/Scripts/menus related scripts/NavigateThroughMenuPages.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateThroughMenuPages : MonoBehaviour
+{
+    public List<GameObject> pages = new List<GameObject>();
+    private int index = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var page in pages)
+        {
+            page.SetActive(false);
+        }
+        pages[index].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextPage()
+    {
+        if (index < pages.Count - 1)
+        {
+            pages[index].SetActive(false);
+            index++;
+            pages[index].SetActive(true);
+        }
+        
+    }
+    
+    public void PreviousPage()
+    {
+        if (index > 0)
+        {
+            pages[index].SetActive(false);
+            index--;
+            pages[index].SetActive(true);
+        }
+    }
+}
