eaeb288e17d0c7ac50839cdbb0ec7b04d68395be
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-11-20 13:50:26 +0100

    Update BarycentricDiagram.cs

--- Assets/Scripts/GameManager related scripts/BarycentricDiagram.cs
+++ Assets/Scripts/GameManager related scripts/BarycentricDiagram.cs
@@ -8,6 +8,7 @@ public class BarycentricDiagram : MonoBehaviour
     public RectTransform pointB;
     public RectTransform pointC;
     public RectTransform pointMarker;
+    public int influenceFactor = 20;
 
 
     public void CalculatePosition(int weightA, int weightB, int weightC)
@@ -22,12 +23,18 @@ public class BarycentricDiagram : MonoBehaviour
 
         if (totalWeight > 0)
         {
+            int minValue = Mathf.Min(weightA, weightB, weightC);
+            weightA = (weightA - minValue )+ influenceFactor;
+            weightB = (weightB - minValue )+ influenceFactor;
+            weightC = (weightC - minValue )+ influenceFactor;
+            
+            totalWeight = weightA + weightB + weightC;
+            
             float a = (float)weightA / totalWeight;
             float b = (float)weightB / totalWeight;
             float c = (float)weightC / totalWeight;
 
-            Vector3 barycentricPosition =
-                a * pointA.anchoredPosition + b * pointB.anchoredPosition + c * pointC.anchoredPosition;
+            Vector3 barycentricPosition = a * pointA.anchoredPosition + b * pointB.anchoredPosition + c * pointC.anchoredPosition;
             pointMarker.anchoredPosition = barycentricPosition;
 
         }
