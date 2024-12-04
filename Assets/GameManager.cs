904cea08abb74371cc819b0bb3f1b8d14980f2c7
Abal798 <158078797+Abal798@users.noreply.github.com>
2024-12-04 12:37:03 +0100

    fast mode

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isInFstMode = false;
+    public float accelerationFactor = 3f;
+    
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
     }
 
+    
+
     // Update is called once per frame
-    void Update()
+    public void ChangeTimeScale()
     {
-        
+        isInFstMode = !isInFstMode;
+        if (isInFstMode)
+        {
+            Time.timeScale = accelerationFactor;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
     }
 }
