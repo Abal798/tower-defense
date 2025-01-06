7bf55bd7aa30fac66a5b62dd606a733dc29c9076
JasonROT <158045104+JasonROT@users.noreply.github.com>
2025-01-06 12:51:58 +0100

    Tutorial Update In Progress
    Début des changements sur les textes suite au Playtest

--- Assets/Scripts/GameManager related scripts/TutorialBehaviour.cs
+++ Assets/Scripts/GameManager related scripts/TutorialBehaviour.cs
@@ -237,7 +237,7 @@ public class TutorialBehaviour : MonoBehaviour
             case 1:
                 ChangeExpression(2, 2);
                 ShowCharacter(2);
-                ModifySpeakingCharacter(2);
+                ModifySpeakingCharacter(2); // Premier dialogue ou Arthur parle
                 ModifyToCurentText();
                 Debug.Log("Arthur parle");
                 break;
@@ -257,72 +257,78 @@ public class TutorialBehaviour : MonoBehaviour
             case 4:
                 ChangeExpression(1,17);
                 ModifyToCurentText();
-                ActivateGameobject(11); // Affiche le SoulsDisplay pour montrer les ressources
+                ActivateGameobject(10); // Affiche le SoulsDisplay pour montrer les ressources
+                Highlight(new Vector2(-70,480),new Vector2(4,1.25f)); // Highlight des Âmes
+                break;
+            case 5:
+                ModifyToCurentText();
                 ActivateGameobject(0); // Affiche les différentes tours et leurs prix
                 ActivateGameobject(1);
                 ActivateGameobject(2);
                 ActivateGameobject(3);
                 ActivateGameobject(4);
                 ActivateGameobject(5);
-               // Highlight(new Vector2(-85,450),new Vector2(3,1)); // Highlight des Âmes à faire au préalable (il manque une intro des différentes ressources)
                 Highlight(new Vector2(-700, -370), new Vector2(6,3.25f)); // Highlight des différentes tours
                 break;
-            
-            case 5:
-                StopHighlighting();
-                ChangeExpression(2,9);
-                ModifyToCurentText();
-                break; //Ajouter un case qui parle des âmes et comment on en obtient (parler bien plus tard du converter)
             case 6:
                 ModifyToCurentText();
-                Highlight(new Vector2(-105, -270), new Vector2(2.5f, 1.25f));
+                StopHighlighting();
+                ChangeExpression(2,9);
                 break;
-            case 7:
+            case 7: // Les Terrains
                 ModifyToCurentText();
-                Highlight(new Vector2(487, -55), new Vector2(1.25f, 1.25f));
-                // Visuel du feu comme mentionné précédemment
-                break;
+                Highlight(new Vector2(-105, -270), new Vector2(2.5f, 1.25f));
+                break; //
             case 8:
                 ModifyToCurentText();
-                Highlight(new Vector2(-220, 380), new Vector2(2.5f, 1.25f));
-                // Le joueur doit placer une tour avant de pouvoir poursuivre.
+                Highlight(new Vector2(487, -55), new Vector2(1.25f, 1.25f));
                 break;
-            
             case 9:
+                ModifyToCurentText();
+                Highlight(new Vector2(-220, 380), new Vector2(2.5f, 1.25f));
+                break;
+            case 10:
+                ModifyToCurentText();
                 ChangeExpression(2, 2);
                 StopHighlighting();
                 ModifySpeakingCharacter(2);
-                ModifyToCurentText();
-                // Le joueur doit placer le reste de ses défenses (c'est pas assez explicit dans le texte)
+                // Le joueur doit placer une tour avant de pouvoir poursuivre.
                 break;
-            case 10:
+            
+            case 11:
+                ModifyToCurentText();
                 ModifySpeakingCharacter(1);
                 ChangeExpression(1, 3);
-                ModifyToCurentText();
-                break;
-            case 11: 
-                ModifyToCurentText();
-                ActivateGameobject(6);
-                ActivateGameobject(11);
-                Highlight(new Vector2(805, 452.5f), new Vector2(3.2f, 0.35f));
-                // Il doit lancer la prochaine vague le prochain dialogue apparaissant après la vague
-                UnlockCamera();
+                // Le joueur doit placer le reste de ses défenses (c'est pas assez explicit dans le texte), en expliquant qu'il peut soit poser plus de tours, soit les améliorer en mettant une autre fiole sur celle-ci.
                 break;
             case 12:
-                ModifySpeakingCharacter(2);
-                ChangeExpression(2, 23);
+                ModifyToCurentText();
                 ChangeExpression(1, 18);
-                ModifyToCurentText();
+                ActivateGameobject(6);
+                ActivateGameobject(10);
+                Highlight(new Vector2(805, 452.5f), new Vector2(3.2f, 0.35f));
                 break;
-            case 13:
-                ChangeExpression(1, 8);
+            case 13: 
+                // Dialogue qui parle des âmes qui sont acquises en prenant les âmes élémentaires des ennemis, selon la tour qui les tues. Une amélioration modifie l'acquisition d'âmes selon les éléments placez dessus
+                // Il doit lancer la prochaine vague le prochain dialogue apparaissant après la vague
                 ModifyToCurentText();
+                UnlockCamera();
                 break;
             case 14:
+                ModifySpeakingCharacter(2);
+                ChangeExpression(1, 23);
                 ChangeExpression(2, 2);
                 ModifyToCurentText();
                 break;
             case 15:
+                ChangeExpression(1, 8);
+                ModifyToCurentText();
+                break;
+            case 16:
+                ChangeExpression(2, 2);
+                ModifyToCurentText();
+                break;
+            case 17:
                 ModifySpeakingCharacter(1);
                 ChangeExpression(1, 17);
                 ModifyToCurentText();
@@ -330,42 +336,47 @@ public class TutorialBehaviour : MonoBehaviour
                     Highlight(new Vector2(-858, 310), new Vector2(1.90f, 0.8f));
                 // Le joueur doit cliquer sur le bouton d'Alchemy pour continuer
                     break;
-            case 16:
+            case 18:
                 ChangeExpression(1, 8);
                 ModifyToCurentText();
                 StopHighlighting();
+                // Parler ici que le premier défini la forme et le 2e slot défini le type de case, les 2 pouvant se cumuler
                 break;
-            case 17:
+            case 19:
                 ChangeExpression(1, 20);
                 ModifyToCurentText();
                 UnlockCamera();
                 Highlight(new Vector2(750, -245), new Vector2(5f, 0.85f));
                 // Le joueur place ensuite son sort nouvellement créer qu'il doit placer pour pouvoir continuer
                 break;
-            case 18:
+            case 20:
                 ChangeExpression(1, 1);
                 ModifyToCurentText();
                 ActivateGameobject(13); // Activation Triangle Barry
                 Highlight(new Vector2(325, 475), new Vector2(1.75f, 1.5f));
+                // Parler un peu plus de ce que ça fait (ou modifie le texte pour dire que ça les renforcent selon l'élément déséquilibré)
                     break;
-            case 19:
+            case 21:
                 ChangeExpression(1, 14);
                 ChangeExpression(2, 18);
                 ModifyToCurentText();
                 Highlight(new Vector2(805, 452.5f), new Vector2(3.2f, 0.35f));
                 // Le joueur doit lancer la prochaine vague pour continuer.
                 break;
-            case 20:
+            case 22:
                 // Une fois la vague terminée
                 ModifySpeakingCharacter(2);
                 ChangeExpression(2, 9);
+                ChangeExpression(1,8);
                 ModifyToCurentText();
                 ActivateAllGameObjects();
                 UnlockCamera();
+                // Si jamais il souhaite supprimer des tours, il y a ce bouton. 
                 break;
-            case 21:
+            case 23:
                 StopTutorial();
                 // Restera le Converter à introduire juste avant
+                // Parler de la conversion si jamais certains éléments manquent, mais que le prix à payer n'est pas une façon optimale de faire comparé à la manière naturelle
                 break;
 
 
